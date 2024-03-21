38513c0e23270dff209ecb5e6929b55df2e9b13d
Muhammad Awais <m.awais@perceptiond.com>
2024-03-21 17:29:09 +0500

    Time: 245 ms (60.88%), Space: 93.2 MB (94.19%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+       sort(intervals.begin(),intervals.end());
+       int previous =0;
+       int ans=0;
+       for(int current=1;current<intervals.size();current++){
+            if(intervals[current][0]<intervals[previous][1]){
+                ans++;
+              
+                if(intervals[current][1]<=intervals[previous][1]){
+                    previous=current;
+                }
+            } 
+            else {
+                previous=current;
+            }
+        
+       }
+       return ans;
+    }
+
+};
